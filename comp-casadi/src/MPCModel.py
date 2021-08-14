e6dfc70c488f68c32832e240adb9663b0e2aa301
Yasmin-Hesham <yasmin98hesham@gmail.com>
2021-08-15 00:52:03 +0200

    1st Trial for path tracking

--- comp-casadi/src/MPCModel.py
+++ comp-casadi/src/MPCModel.py
@@ -6,28 +6,33 @@ import numpy as np
 X = ca.SX.sym('X', n_states, N+1)
 # controls: matrix containing all control actions over all time steps (each column is an action vector)
 U = ca.SX.sym('U', n_controls, N)
-# path parameter: TODO: write comment
-K = ca.SX.sym('K', 1, N)
+# path parameter: TODO: revise it (N-->2)
+K = ca.SX.sym('K', 1, N+1)
 # parameters: column vector for storing initial state, target state, initial controls and path tracking 
-P = ca.SX.sym('P', (n_states+n_controls) + (order+1)*2)
+P = ca.SX.sym('P', (n_states+n_controls) + (order+1)*2 + 1)
+
+x_coeff = P[n_states+n_controls: n_states+n_controls+(order+1)]
+y_coeff = P[n_states+n_controls+(order+1):-1]
 
 ''' Constructing cost function '''
 cost_fn = 0  # initialize cost
 # accumulate state-error cost
-for i in range(N):
+for i in range(N+1):
     #state_error = state - target_state
     pathX_error = X[0, i] - path_poly(
-                                        K[0, i], 
-                                        P[n_states+n_controls+1 : n_states+n_controls+1+(order+1)]
-                                       )
+                                      K[0, i], 
+                                      x_coeff
+                                      )
     pathY_error = X[1, i] - path_poly(
-                                        K[0, i], 
-                                        P[n_states+n_controls+1+(order+1):]
-                                        )
+                                      K[0, i],
+                                      y_coeff
+                                      )
+    theta_error = X[2, i]
     # pathTheta_error = X[2, i+1] - P[2*n_states - 1]
     # add the weighted pathX error to the total cost
     cost_fn += pathX_error.T @ Q_x @ pathX_error
     cost_fn += pathY_error.T @ Q_y @ pathY_error
+    cost_fn += theta_error.T @ Q_theta @ theta_error
 
 # accumulate control-error cost
 for i in range(N):
@@ -36,9 +41,8 @@ for i in range(N):
     cost_fn += control_error.T @ R @ control_error
 
 # accumulate path tracking cost
-for i in range(N):
-    path_error = 1 - K[:, i]
-    cost_fn += path_error.T @ H @ path_error
+path_error = 1 - K
+cost_fn += (path_error @ path_error.T) * H
  
 
 ''' Constructing constraints '''
@@ -47,24 +51,50 @@ lbg = ca.DM()  # initialize lowerbounds vector
 ubg = ca.DM()  # initialize upperbounds vector
 
 # adding physical constraints (next state must adhere to the robot model)
-g = ca.vertcat(g, X[:, 0] - P[:n_states])                                    # constraints in the equation
-# g = ca.vertcat(g, K[:, 0] - P[n_states+n_controls : n_states+n_controls+1])  # const[:n_statesraints in the equation
+# equality constraints
+g = ca.vertcat(g, X[:, 0] - P[:n_states])                                    
 lbg = ca.vertcat(lbg, [0] * (n_states))
 ubg = ca.vertcat(ubg, [0] * (n_states))
-# TODO: check if we need to add constraints for poly in X and y
+
+# path constraints
+g = ca.vertcat(g, K[0, 0] - P[-1])  
+lbg = ca.vertcat(lbg, [0])
+ubg = ca.vertcat(ubg, [0])
+
+# g = ca.vertcat(g, (K[0, 1:] - K[0, :-1]).T)
+# lbg = ca.vertcat(lbg, [0] * (N-1))
+# ubg = ca.vertcat(ubg, [ca.inf] * (N-1))
+
+# for i in range(N):
+#     # dK/dX * dX/dt = dK/dY * dY/dt
+#     # dY/dK * dX/dt = dX/dK * dY/dt
+#     dxdk_coeff = x_coeff[:-1] * np.arange(order, 0, -1)
+#     dxdk = path_poly(K[0, i], dxdk_coeff)
+
+#     dydk_coeff = y_coeff[:-1] * np.arange(order, 0, -1)
+#     dydk = path_poly(K[0, i], dydk_coeff)
+
+#     g = ca.vertcat(g, (dydk * U[0, i]) - (dxdk * U[1, i]))
+#     lbg = ca.vertcat(lbg, [0])
+#     ubg = ca.vertcat(ubg, [0])
+
+#    # K_next = K_current + K_dot (K, V_x, x_coeff) * delta_t
+#     # or
+#     # K_next = K_current + K_dot (K, V_y, y_coeff) * delta_t 
+#     g = ca.vertcat(g, K[0, i+1] - K[0, i] + dKdT(K[0, i], U[1, i], P[n_states+n_controls+(order+1): -1]) * sampling_time)
+#     lbg = ca.vertcat(lbg, [0]) # TODO: problem here?
+#     ubg = ca.vertcat(ubg, [0])
 
 for i in range(N):
     state = X[:, i]
     controls = U[:, i]
     next_state = X[:, i+1]
-
     # 4th order Runge-Kutta discretization
     k1 = derivatives(state, controls)
     k2 = derivatives(state + sampling_time/2*k1, controls)
     k3 = derivatives(state + sampling_time/2*k2, controls)
     k4 = derivatives(state + sampling_time * k3, controls)
     predicted_state = state + (sampling_time / 6) * (k1 + k2*2 + k3*2 + k4)
-
     # add constraint for meccanum wheeled: next_state == predicted_state
     g = ca.vertcat(g, next_state - predicted_state)
     lbg = ca.vertcat(lbg, [0] * n_states)
@@ -108,18 +138,21 @@ solver = ca.nlpsol('solver', 'ipopt', nlp_prob, options)
 
 #######################################################
 
-
+# TODO: needs edits
+ 
 class MPC:
     X0 = ca.DM.zeros((n_states * (N+1), 1))
     U0 = ca.DM.zeros((n_controls * N, 1))
-    K0 = ca.DM.zeros((N, 1))
+    K0 = ca.DM.zeros((N+1, 1))
 
     def compute(self, initial_state, initial_controls, x_coeffs, y_coeffs, isDifferential=False):
         args['p'] = ca.vertcat(
             initial_state,          # current state
-            self.U0[:n_controls],   # current controls
+            # self.U0[:n_controls], # current controls
+            initial_controls,       # current controls
             x_coeffs,               # coefficients of x in path in terms of k
-            y_coeffs                # coefficients of y in path in terms of k
+            y_coeffs,               # coefficients of y in path in terms of k
+            self.K0[1]
         )
 
         # optimization variable current state
@@ -133,25 +166,26 @@ class MPC:
         self.X0[-n_states:] = self.X0[-2*n_states:-n_states]
 
         # Same as the states.
-        self.U0[:-n_controls] = self.U0[n_controls:]
+        self.U0[:n_controls] = initial_controls
+        self.U0[n_controls:-n_controls] = self.U0[2*n_controls:]
         self.U0[-n_controls:] = self.U0[-2*n_controls:-n_controls]
 
         self.K0[:-1] = self.K0[1:]
-        self.K0[-1] = self.K0[-2]
+        self.K0[-1:] = self.K0[-2:-1]
 
         args['x0'] = ca.vertcat(
             self.X0,
             self.U0,
             self.K0
-        )
+        ) 
 
         if isDifferential:
-            args['lbx'][n_states*(N+1)+0:: n_controls] = 0
-            args['ubx'][n_states*(N+1)+0:: n_controls] = 0
+            args['lbx'][n_states*(N+1):n_states*(N+1) + n_controls*N: n_controls] = 0
+            args['ubx'][n_states*(N+1):n_states*(N+1) + n_controls*N: n_controls] = 0
 
         else:
-            args['lbx'][n_states*(N+1)+0:: n_controls] = -vx_max
-            args['ubx'][n_states*(N+1)+0:: n_controls] = vx_max
+            args['lbx'][n_states*(N+1):n_states*(N+1) + n_controls*N: n_controls] = -vx_max
+            args['ubx'][n_states*(N+1):n_states*(N+1) + n_controls*N: n_controls] = vx_max
 
         sol = solver(
             x0=args['x0'],
@@ -164,6 +198,6 @@ class MPC:
         self.X0 = sol['x'][:n_states * (N+1)]
         self.U0 = sol['x'][n_states * (N+1) : (n_states * (N+1)) + (n_controls * N)]
         self.K0 = sol['x'][(n_states * (N+1)) + (n_controls * N):]
-        print(self.K0)
+        print(f"K: {self.K0[:5]}")
 
         return self.U0[:n_controls]
--- comp-casadi/src/MPCTools.py
+++ comp-casadi/src/MPCTools.py
@@ -1,18 +1,20 @@
+# TODO: K--> carrot and States--> donkey
+
 import casadi as ca
 from casadi import sin, cos, pi
 import numpy as np
  
 ''' Constants '''
 # Costs
-Q_x = 150
-Q_y = 150
+Q_x = 50
+Q_y = 50
 Q_theta = 3000
 R1 = R2 = R3 = 0.5     # speed cost
 A1 = A2 = A3 = 0       # Acceleration
-H = 1e5                # path parameter cost
+H = 1e7
  
 # MPC parameters
-sampling_time = 1   # time between steps in seconds
+sampling_time = 0.5  # time between steps in seconds
 N = 100              # number of look ahead steps
 
 # MPC limits
@@ -40,6 +42,8 @@ def path_poly(sym, coeffs):
 x = ca.SX.sym('x')
 y = ca.SX.sym('y')
 theta = ca.SX.sym('theta')
+k = ca.SX.sym('k')
+y_coeff = ca.SX.sym('y_coeff', order+1)
 states = ca.vertcat(
     x,
     y,
@@ -72,40 +76,44 @@ rot_3d_z = ca.vertcat(
     ca.horzcat(sin(theta),  cos(theta), 0),
     ca.horzcat(         0,           0, 1)
 )
- 
+
 state_change_rate = rot_3d_z @ controls
- 
+
 derivatives = ca.Function('derivatives', [states, controls], [state_change_rate])
 
+# y = c0*k^n + c1*k^(n-1) + ... + c(n-1)*k^1 + cn*k^0
+# dy/dk = n*c0*k^(n-1) + (n-1)*c1*k^(n-2) + ... + c(n-1)*k^0 
+dydk_coeff = y_coeff[:-1] * np.arange(order, 0, -1)
+# dydk = pathpoly(k, dydk_coeff)
+# dK/dt = dK/dY * dY/dt
+# dK/dt = dY/dt / dY/dK
+k_change_rate = V_y / path_poly(k, dydk_coeff)
+dKdT = ca.Function('dKdT', [k, V_y, y_coeff], [k_change_rate])
 
 ''' Defining Upper and Lower Bounds '''
 # initialize boundaries arrays
-lbx = ca.DM.zeros((n_states*(N+1) + n_controls*N + N, 1))
-ubx = ca.DM.zeros((n_states*(N+1) + n_controls*N + N, 1))
- 
+lbx = ca.DM.zeros((n_states*(N+1) + n_controls*N + N+1, 1))
+ubx = ca.DM.zeros((n_states*(N+1) + n_controls*N + N+1, 1))
+
 # state lower bounds
 lbx[0: n_states*(N+1): n_states] = x_min      # X lower bound
 lbx[1: n_states*(N+1): n_states] = y_min      # Y lower bound
 lbx[2: n_states*(N+1): n_states] = theta_min  # theta lower bound
- 
 # state upper bounds
 ubx[0: n_states*(N+1): n_states] = x_max      # X upper bound
 ubx[1: n_states*(N+1): n_states] = y_max      # Y upper bound
 ubx[2: n_states*(N+1): n_states] = theta_max  # theta upper bound
- 
-# control lower bounds
+
+# control lower bounds 
 lbx[n_states*(N+1)+0: n_states*(N+1)+ n_controls*N: n_controls] = -vx_max  # Vx lower bound
 lbx[n_states*(N+1)+1: n_states*(N+1)+ n_controls*N: n_controls] = -vy_max  # Vy lower bound
-lbx[n_states*(N+1)+2: n_states*(N+1)+ n_controls*N: n_controls] = 0   # w lower bound
- 
+lbx[n_states*(N+1)+2: n_states*(N+1)+ n_controls*N: n_controls] = -w_max   # w lower bound
 # control upper bounds
 ubx[n_states*(N+1)+0: n_states*(N+1)+ n_controls*N: n_controls] = vx_max   # Vx upper bound 
 ubx[n_states*(N+1)+1: n_states*(N+1)+ n_controls*N: n_controls] = vy_max   # Vy upper bound
-ubx[n_states*(N+1)+2: n_states*(N+1)+ n_controls*N: n_controls] = 0    # w upper bound
+ubx[n_states*(N+1)+2: n_states*(N+1)+ n_controls*N: n_controls] = w_max   # w upper bound
 
 # path lower bounds
-lbx[n_states*(N+1)+ n_controls*N:] = -vx_max  # Vx lower bound
- 
+lbx[n_states*(N+1)+ n_controls*N:] = 0 
 # path upper bounds
-ubx[n_states*(N+1)+ n_controls*N:] = vx_max   # Vx upper bound 
-
+ubx[n_states*(N+1)+ n_controls*N:] = 1  
